1916b23b6c0d4026ad498927bb49ddd3d8d23573
Mauro <157161224+Mauro-devs@users.noreply.github.com>
2025-11-23 00:33:03 -0300

    Delete back-end directory

--- back-end/docs/diagrama_er.md
+++
@@ -1,8 +0,0 @@
-Exemplo de mermaid para poder jogar aqui (ou usar uma imagem tlvz)
-
-```mermaid
-graph TD;
-    A-->B;
-    A-->C;
-    B-->D;
-    C-->D;
--- back-end/src/main/java/community_health/com/communityHealth/usuario/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package community_health.com.communityHealth.usuario.repository;
-
-import community_health.com.communityHealth.usuario.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
